cddcbae32477a85bfac69deda0b1390c0d7f9c2f
Ivana <ivaptt@gmail.com>
2023-04-23 16:20:25 +0200

    added navigation, updated html, styles and js

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
 $(document).ready(function () {
-  const body = $("body");
-  const header = $("header");
+  const hamburger = $(".nav-toggler");
+  const headWrapper = $(".header-wrapper");
+  const nav = $(".navigation");
+  let width = screen.width;
 
-  $(window).scroll(function () {
-    console.log("skrol");
+  hamburger.click(function () {
+    headWrapper.toggleClass("nav-open");
   });
+
+  if (width >= 1200 && nav.css("display", "none")) {
+    nav.css("display", "flex");
+  }
+
+  if (width >= 1200 && headWrapper.hasClass("nav-open")) {
+    headWrapper.removeClass("nav-open");
+  }
 });
